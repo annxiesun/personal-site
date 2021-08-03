8d7b9b092a17de72691bdf41626a69b049820f93
Annie <annie.sun@enzuzo.com>
2021-08-03 11:11:09 -0400

    remove hardcode port

--- server/index.js
+++ server/index.js
@@ -57,6 +57,6 @@ app.post(sendRoute, function (req, res) {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port: ${port}`);
+app.listen(process.env.PORT , () => {
+  console.log(`Server is running}`);
 });
