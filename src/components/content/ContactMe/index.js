9d9ab1ca62a7b9eab3a5d8354acbe7f1111c8a7e
Annie <annie.sun@enzuzo.com>
2021-08-04 00:22:02 -0400

    added submit behaviour to contact form

--- src/components/content/ContactMe/index.js
+++ src/components/content/ContactMe/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Lottie from "react-lottie";
+
+import paperAirplane from '../../../animations/airplane.json';
 import './style.css';
 
-function Input({ value, label, updateInfo }) {
+function Input({ id, value, label, disabled, updateInfo }) {
   return (
     <div className="contact-input-container">
       <div className={`contact-label ${value === '' ? 'contact-label-unfocused' : 'contact-label-focused'}`}>{label}</div>
@@ -10,6 +13,8 @@ function Input({ value, label, updateInfo }) {
         className="contact-input"
         onChange={(e) => updateInfo(label, e.target.value)}
         value={value}
+        disabled={disabled}
+        id={`${label}-input`}
       />
     </div>
   );
@@ -19,9 +24,46 @@ function ContactMe({ id }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [sent, setSent] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [isStopped, setIsStopped] = useState(true);
 
-  const submitEmail = async (e) => {
+  const submitEmail = (e) => {
     e.preventDefault();
+
+    const inputs = [
+      { label: 'name', value: name },
+      { label: 'email', value: email },
+      { label: 'message', value: message }
+    ];
+
+    setSubmitted(true);
+
+    let invalid = false;
+    const checkInput = (label, value) => {
+      const shakeInput = (inputEl) => {
+        setTimeout(() => { inputEl.classList.add('shake') }, 500);
+        inputEl.classList.remove('shake');
+      }
+
+      if (!validateInfo(label, value)) {
+        const inputEl = document.getElementById(`${label}-input`);
+        shakeInput(inputEl);
+        invalid = true;
+        setSubmitted(false);
+      }
+    }
+
+    inputs.forEach((input) => {
+      checkInput(input.label, input.value);
+    });
+
+    if (invalid) {
+      return;
+    }
+
+    setIsStopped(false);
+    setTimeout(() => { setSent(true) }, 2000);
 
     const route = encodeURI("/send");
     const response = await fetch(route, {
@@ -62,10 +104,27 @@ function ContactMe({ id }) {
     }
   }
 
+  const validateInfo = (label, value) => {
+    if (value === '') return false;
+    switch (label) {
+      case 'email':
+        if (value.indexOf('@') === -1) {
+          return false;
+        }
+    }
+    return true;
+  }
+
   const inputs = [
     { label: 'name', value: name },
     { label: 'email', value: email },
   ];
+
+  const paperAirplaneAnimation = {
+    animationData: paperAirplane,
+    loop: false,
+    autoplay: false,
+  };
 
   return (
     <div id={id}>
@@ -73,9 +132,14 @@ function ContactMe({ id }) {
         <div className="h3 bold">Contact Me</div>
         <div className="body header-text">making ideas come to life from design to implemention</div>
         <br />
-        <form>
+        {!sent && <form className="form-container">
           {inputs.map((input) => (
-            <Input value={input.value} label={input.label} updateInfo={updateInfo} />
+            <Input
+              value={input.value}
+              label={input.label}
+              disabled={submitted}
+              updateInfo={updateInfo}
+            />
           ))}
           <div className="contact-textarea-container">
             <textarea
@@ -83,10 +147,17 @@ function ContactMe({ id }) {
               className="contact-textarea"
               onChange={(e) => updateInfo('message', e.target.value)}
               value={message}
+              disabled={submitted}
+              id="message-input"
             />
           </div>
-          <button onClick={submitEmail}>Click me</button>
-        </form>
+          <button className="submit-button" onClick={submitEmail} disabled={submitted}>
+            Send
+          </button>
+          <div className="paper-anim">
+            <Lottie options={paperAirplaneAnimation} isStopped={isStopped} isClickToPauseDisabled={true} />
+          </div>
+        </form>}
       </div>
     </div>
   );
